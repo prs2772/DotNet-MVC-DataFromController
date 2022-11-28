064e3417c59d24900084a6c2d6fe4c1b4a6adedf
ParisRamírezS <pramirezs2705@gmail.com>
2022-11-27 18:08:36 -0600

    Second commit

---
+++ Coevaluacion4/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Coevaluacion4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
